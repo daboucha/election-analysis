a8631956e049a6bd61eb724083ac2a6b7a1ad505
Danny <89036003+daboucha@users.noreply.github.com>
2021-09-07 19:56:15 -0400

    Update

--- PyPoll.py
+++ PyPoll.py
@@ -11,7 +11,7 @@ import csv
 import os
 
 # Assign a variable to load a file from a path.
-file_to_load = os.path.join("resources", "election_results.csv")
+file_to_load = os.path.join("D:/Bootcamp/git clones/election-analysis/resources/election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("D:/Bootcamp/git clones/election-analysis/election_analysis.txt")
 
