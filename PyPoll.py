1c2c679901a90b2ac2880fc8e30248e71dfa99dc
Danny <89036003+daboucha@users.noreply.github.com>
2021-09-07 16:04:13 -0400

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -11,9 +11,9 @@ import csv
 import os
 
 # Assign a variable to load a file from a path.
-file_to_load = os.path.join("resources", "election_results.csv")
+file_to_load = os.path.join("D:/Bootcamp/git clones/election-analysis/resources/election_results.csv")
 # Assign a variable to save the file to a path.
-file_to_save = os.path.join("election_analysis.txt")
+file_to_save = os.path.join("D:/Bootcamp/git clones/election-analysis/election_analysis.txt")
 
 # Initialize a total vote counter.
 total_votes = 0
@@ -29,7 +29,7 @@ winning_count = 0
 winning_percentage = 0
 
 # Open the election results and read the file.
-with open("D:/Bootcamp/git clones/election-analysis/resources/election_results.csv") as election_data:
+with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
     
     # Read the header row.
@@ -52,6 +52,14 @@ with open("D:/Bootcamp/git clones/election-analysis/resources/election_results.c
 
         # Increment the existing candidate's vote count.
         candidate_votes[candidate_name] += 1
+
+    # Print the header and total votes.
+    #    election_results = (
+    #    f"\nElection Results\n"
+    #    f"-------------------------\n"
+    #    f"Total Votes: {total_votes:,}\n"
+    #    f"-------------------------\n")
+    #print(election_results, end="")
     
     # Determine the percentage of votes for each candidate by looping through the counts.
     # Iterate through the candidate list.
@@ -63,7 +71,7 @@ with open("D:/Bootcamp/git clones/election-analysis/resources/election_results.c
         vote_percentage = (float(votes) / float(total_votes))*100
 
         # Print the candidate name and percentage of votes
-        print(f"{candidate_name}: recieved {vote_percentage:.2f}% of the vote.")
+        #candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})")
 
         # Determine winning vote count and candidate
         # Determine if the votes are greater than the winning count.
@@ -76,5 +84,51 @@ with open("D:/Bootcamp/git clones/election-analysis/resources/election_results.c
             winning_candidate = candidate_name
 
     # Printing the output.
-    print(f"The winning candidate is {winning_candidate} with {winning_percentage:.2f}% of the vote.")
+    winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+#print(winning_candidate_summary)
+
+with open(file_to_save, "w") as txt_file:
+    # Print the final vote count to the terminal.
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    # Save to text file.
+    txt_file.write(election_results)
+
+    for candidate_name in candidate_votes:
+        # Retrieve vote count of a candidate.
+        votes = candidate_votes[candidate_name]
+
+        # Calculate the percentage of votes.
+        vote_percentage = (float(votes) / float(total_votes))*100
+
+        # Print the candidate name and percentage of votes
+        # Save to text file.
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})")
+        txt_file.write(candidate_results)
+        # Determine winning vote count, winning percentage, and winning candidate.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_candidate = candidate_name
+            winning_percentage = vote_percentage
+    # Print the winning candidate's results to the terminal.
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    # Save the winning candidate's results to the text file.
+    txt_file.write(winning_candidate_summary)
+    
+
+
+
         
